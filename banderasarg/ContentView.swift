c11f114d502f0dff56b72d7b394134e6cc68629c
Fran Sosa <franciscososa91@gmail.com>
2019-06-23 15:10:52 -0300

    Initial Commit

---
+++ banderasarg/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  banderasarg
+//
+//  Created by Francisco sosa on 23/06/2019.
+//  Copyright © 2019 watafaka-dev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
